8ee7151067caf3bb54434b292c21dbfc724059a5
bjahng <bjj34@yahoo.com>
2016-03-23 16:29:14 -0700

    Interactive version

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 
-	
+//mark items as done
+	$('.list ul').on('click', 'li', function() {
+		$(this).toggleClass('linethrough');
+		$(this).children('.box').toggleClass('done');
+	});
+
+//delete items
+	$('.list ul').on('click', '.delete', function() {
+		$(this).parent().remove();
+	});
+
 });
